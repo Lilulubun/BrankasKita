25d740f360460c5f0ab0218b684403d63721efbe
Lilulubun <nawalhaq11@gmail.com>
2025-07-01 17:02:40 +0700

    fix forgot password

--- src/app/components/AuthProvider/AuthProvider.tsx
+++ src/app/components/AuthProvider/AuthProvider.tsx
@@ -14,7 +14,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-  const pathname = usePathname();
+  
 
   useEffect(() => {
     // This function runs once to get the initial session
